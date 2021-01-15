a4ff0aae6e330c7116e49f22d0f3df863922c7a9
EnderMo <48214360+EnderMo@users.noreply.github.com>
2021-01-15 19:43:19 +0800

    Add files via upload

---
+++ VertexUI/framework.h
@@ -0,0 +1,20 @@
+﻿/*
+* framework.h
+* 
+* 包含WINAPI运行库,同时包含VertexUI预处理宏
+*/
+
+#pragma once
+
+#include "targetver.h"
+// Windows 头文件
+#include <windows.h>
+// C 运行时头文件
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#define SimpleCode //更简洁的代码
+
+//#define VertexUI_LightMode  //明亮模式
