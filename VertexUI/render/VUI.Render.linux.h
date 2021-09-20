bff67c0eb0422f1ab78be16987fe7831cc864cf5
李家骏 <geometry2019@outlook.com>
2021-09-20 19:02:43 +0800

    颜色编写完毕

--- VertexUI/render/VUI.Render.linux.h
+++ VertexUI/render/VUI.Render.linux.h
@@ -3,7 +3,5 @@ class VUI_Render:public VUI_Render_ORIGIN{
     void DrewRect(int x,int y,int width,int height,int r,int g,int b);
 
 
-    
-
 
 }
--- VertexUI/render/VUI.Render.origin.h
+++ VertexUI/render/VUI.Render.origin.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../../framework.h"
+
 class VUIColor
 {
 private:
@@ -7,24 +8,33 @@ private:
     float ca;
 
 public:
-    inline VUIColor(int r, int g, int b, float a = 1)
-    {
+    inline VUIColor(int r, int g, int b, float a = 1){
         this->ca = a;
         this->cr = r;
         this->cb = b;
         this->cg = g;
     }
-    inline VUIColor(char *hexColor)
-    {
+    inline VUIColor(const char *hexColor){
         switch (strlen(hexColor))
         {
+
         case 7:
+        //格式#ffffff
+
             char *dest=new char[6];
             memcpy(dest,hexColor+1,7);
+            this->cr=std::stoi(std::string(dest).substr(0,2),0,16);
+            this->cg=std::stoi(std::string(dest).substr(2,2),0,16);
+            this->cb=std::stoi(std::string(dest).substr(4,2),0,16);
             break;
         case 9:
+        //格式#ffffffff
             char *dest=new char[8];
             memcpy (dest,hexColor+1,8);
+             this->cr=std::stoi(std::string(dest).substr(0,2),0,16);
+            this->cg=std::stoi(std::string(dest).substr(2,2),0,16);
+            this->cb=std::stoi(std::string(dest).substr(4,2),0,16);
+            this->ca=std::stoi(std::string(dest).substr(6,2),0,16)/256;
 
             break;
         default:
@@ -32,13 +42,14 @@ public:
             this->cr =0;
             this->cb = 0;
             this->cg = 0;
+
         }
+
     }
     inline ~VUIColor() {}
 };
 
-class VUI_Render_ORIGIN
-{
+class VUI_Render_ORIGIN{
     virtual void DrewRect(int x, int y, int width, int height) = 0;
     virtual void PutPixel(int x, int y) = 0;
     //Render需要绑定主窗口的画布，不同系统的画布不一样，比如windows的GDI就是HDC
@@ -47,5 +58,9 @@ class VUI_Render_ORIGIN
     VUI_Render_ORIGIN();
     ~VUI_Render_ORIGIN();
 };
+
 //这里定义了Render的基本格式
 //在其他平台的文件中，只需要把这个类中所有的虚函数实现就好
+
+
+
